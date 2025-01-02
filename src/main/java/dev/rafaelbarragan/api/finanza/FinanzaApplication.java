67ab8d8baf990bfbf9d1dc21948e8690975d7f1e
Rafael Barragán Acevedo <70608879+rafael20044@users.noreply.github.com>
2025-01-02 18:28:06 -0500

    first commit

---
+++ src/main/java/dev/rafaelbarragan/api/finanza/FinanzaApplication.java
@@ -0,0 +1,13 @@
+package dev.rafaelbarragan.api.finanza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/rafaelbarragan/api/finanza/FinanzaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rafaelbarragan.api.finanza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
